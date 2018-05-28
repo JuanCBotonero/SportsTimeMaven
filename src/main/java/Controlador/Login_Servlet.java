1b0afc6ce27d6c1ecdd04e7c1238766527ba3bda
Juan Camilo Botonero Rodriguez <jcbotonero97@hotmail.com>
2018-05-28 13:18:42 -0500

    Modificacion con response de jsp

--- src/main/java/Controlador/Login_Servlet.java
+++ src/main/java/Controlador/Login_Servlet.java
@@ -44,7 +44,7 @@ public class Login_Servlet extends HttpServlet {
 
                 for (int i = 0; i < crr.findAll().size(); i++) {
 
-                    if (textoEncriptadoConMD5.equals(crr.findAll().get(i).getPassword()) && UserName.equals(crr.findAll().get(i).getName())) {
+                    if (Password.equals(crr.findAll().get(i).getPassword()) && UserName.equals(crr.findAll().get(i).getName())) {
                         Register reg = new Register();
                         reg.setTemp(i);
                         misession.setAttribute("usuario", "Entrenador");
@@ -57,7 +57,7 @@ public class Login_Servlet extends HttpServlet {
 
                 for (int i = 0; i < crr2.findAll().size(); i++) {
 
-                    if (textoEncriptadoConMD5.equals(crr2.findAll().get(i).getPassword()) && UserName.equals(crr2.findAll().get(i).getName())) {
+                    if (Password.equals(crr2.findAll().get(i).getPassword()) && UserName.equals(crr2.findAll().get(i).getName())) {
                         Register reg = new Register();
                         reg.setTemp(i);
                         misession.setAttribute("usuario", "Deportista");
